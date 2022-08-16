bd5880fbc203a3cdcb60c0c0d73c491247b29843
Camilo <52603058+ctatis1@users.noreply.github.com>
2022-08-15 20:41:24 -0500

    GET all users & CREATE

--- routes/user.routes.go
+++ routes/user.routes.go
@@ -1,1 +1,35 @@
 package routes
+
+import (
+	"encoding/json"
+	"github/ctatis1/gorm-restapi/database"
+	"github/ctatis1/gorm-restapi/models"
+	"net/http"
+)
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var users []models.User
+
+	database.DB.Find(&users)
+
+	json.NewEncoder(w).Encode(&users)
+}
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get User"))
+}
+func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	createdUser := database.DB.Create(&user)	
+	err := createdUser.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))	
+	}
+
+	json.NewEncoder(w).Encode(&user)
+}
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Delete User"))
+}
